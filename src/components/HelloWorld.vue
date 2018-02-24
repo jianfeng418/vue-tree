d4529511efe998b6b03f886400649a8e509cf3e3
jianfeng418 <jianfeng418@sina.com>
2018-02-24 18:03:45 +0800

    first version

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <vtree :treeData='datas' ref='vtree' :clickFun='clickNode'></vtree>
+
+    <button @click='findSelected'>获取选中项</button>
+
+  </div>
+</template>
+
+<script>
+  import mytree from './mytree'
+  import vtree from './v-tree'
+
+ 
+export default {
+  name: 'HelloWorld',
+  methods:{
+      findParent(){
+          console.log(this.$refs)
+      },
+      findSelected(){
+          console.log(this.$refs.vtree.getSelectedNode());
+      },
+      clickNode($event){
+        console.log('you clicked '+ $event.target.id)
+      }
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      datas:[
+        {text:'xiaoming',
+          id:'1',
+          children:[
+              {text:'1-1',
+                id:'1-1',
+                children:[
+                  {text:'1-1-1',
+                    id:'1-1-1',
+
+                  }]
+              
+              }]
+        },
+        {text:'hua',
+          id:'2',
+          children:[{
+            text:'2-2',
+            id:'2-2'
+          }]
+        },
+        {text:'hua',
+          id:'3',
+          children:[{
+            text:'3-3',
+            id:'3-3'
+          }]
+        }
+        ]
+    }
+  },
+  components:{mytree,vtree}
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/v-tree.vue
@@ -0,0 +1,145 @@
+<template>
+	<div class='cus_vtree_wrap' @click.capture='clickNodeWrap'>
+		
+			<v-tree-item :treeData='treeData' :clickFun='clickFun' ></v-tree-item>
+		
+	</div>
+</template>
+
+<script>
+	import Vue from 'vue'
+	export default{
+		name:'v-tree',
+		props:['treeData','clickFun'],
+		data(){
+			return {
+				userData:this.treeData
+			}
+		},
+		mounted(){
+			this.initData(true,true);
+			console.log(this)
+		},
+		methods:{
+			initData(expendInit,activeInit){
+				var modifyDataFun = function(datas){
+					if(datas){
+						datas.forEach( (m,index)  => {
+							if(expendInit){
+								Vue.set(m,'expend',true);
+							}
+							if(activeInit){
+								Vue.set(m,'active',false);
+							}
+							if(index === datas.length - 1){
+								Vue.set(m,'last',true);
+							}
+							if(m.children){
+								modifyDataFun(m.children);
+							}
+						})
+					}
+				}
+				modifyDataFun(this.userData)
+			},
+			clickNodeWrap(){
+				this.initData(false,true);
+			},
+			getSelectedNode(){
+				var resultNode = null;
+				var getSelectedFun = (datas) => {
+					if(datas){
+						datas.forEach((m) => {
+							if(m.active === true){
+								resultNode = m;
+								return;
+							}
+							if(m.children){
+								getSelectedFun(m.children)
+							}
+						})
+					}
+				};
+				getSelectedFun(this.treeData);
+				return resultNode;
+			}
+		},
+		components:{'v-tree-item':{
+			name:'v-tree-item',
+			template:'<ul class="cus_tree_ul" :class="{cus_tree_ulLine:(treeData && treeData.length)}"><li v-for="item in treeData"><div class="cus_item_content" @click="clickNode(item)" @click.cus="clickFun" :id= item.id :class="{active:item.active}">'+
+			'<span class="treeExpendBtn" @click.stop="toggleNode(item)" :class="{butopen:item.expend,btnclose:!item.expend,lastLine:item.last&&!item.children}"></span>{{item.text}} </div>'+
+			'<v-tree-item :treeData="item.children" v-if="item.expend" :clickFun="clickFun"></v-tree-item> </li></ul>',
+
+			methods:{
+				clickNode(item){
+					
+					item.active = true;
+					
+				},
+				toggleNode(item){
+					item.expend = !item.expend;
+					item.active = true;
+				}
+			},
+			props:['treeData','clickFun']
+
+		}}
+
+	}
+</script>
+
+<style>
+	.cus_vtree_wrap ul:first-child{
+		background-image:none;
+	}
+	 ul.cus_tree_ul{
+		padding-left:16px;
+		background-image:url('../images/index.gif');
+		background-position:4px 10px;
+		background-repeat:repeat-y;
+	}
+	li:last-child ul.cus_tree_ul:last-child{
+		background-image:none;
+	}
+
+	ul.cus_tree_ul li{
+		text-align:left;
+		cursor:pointer;
+		list-style:none;
+		
+	}
+
+	.cus_item_content span.treeExpendBtn{
+		display:inline-block;
+		width:16px;
+		height:16px;
+		margin-right:6px;
+		background-image:url('../images/zTreeStandard.png');
+		
+	}
+	.cus_item_content span.treeExpendBtn.butopen{
+		background-position:-92px -36px
+	}
+	.cus_item_content span.treeExpendBtn.butopen.lastLine{
+		background-position:-56px -36px
+	}
+
+	.cus_item_content span.treeExpendBtn.btnclose{
+		background-position:-74px -36px
+	}
+	.cus_item_content span.treeExpendBtn.btnclose.lastLine{
+		background-position:-56px -36px
+	}
+
+	ul.cus_tree_ul li .cus_item_content{
+		padding:4px;
+	}
+	ul.cus_tree_ul li .cus_item_content:hover{
+		background:#8f83e1;
+		color:white
+	}
+	ul.cus_tree_ul li .cus_item_content.active{
+		background:#7663f8;
+		color:white;
+	}
+</style>
